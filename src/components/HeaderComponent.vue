f2d7d064b2e63e0466b4810ee180cf6a3dcd7447
Victor <victordevos.pro@gmail.com>
2024-11-02 16:20:51 +0100

    fix bug au click du header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,15 +5,19 @@
 
 
 
-        <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button 
+        class="navbar-toggler ms-auto" 
+        type="button" 
+        @click.stop="toggleMenu"
+        aria-expanded="isMenuOpen" 
+        aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <div class="collapse navbar-collapse justify-content-end" :class="{ show: isMenuOpen }" id="navbarNav">
           <div class="navbar-nav">
             <router-link class="nav-link nav-anchor" :to="'/'" @click="closeMenu">À mon propos </router-link>
             <div class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+      <a class="nav-link-projects dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
          Projets
       </a>
       <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -25,7 +29,7 @@
    </div>
             <router-link class="nav-link nav-anchor" :to="'/experiences'" @click="closeMenu">Éxperiences</router-link>
             <router-link class="nav-link nav-anchor" :to="'/contact'" @click="closeMenu">Contact</router-link>
-            <a class="nav-link link-cv" target="_blank" href="/CV_Victor_De_Vos.pdf">Voir Mon CV</a>
+            <a class="nav-link link-cv no-underline" target="_blank" href="/CV_Victor_De_Vos.pdf">Voir Mon CV</a>
           </div>
         </div>
       </div>
@@ -33,15 +37,19 @@
   </template>
 
 <script>
-import { Collapse } from 'bootstrap'; // Import explicite de Collapse
 
     export default {
+      data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
       methods: {
+        toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
     closeMenu() {
-      const navbarCollapse = document.getElementById('navbarNav');
-      if (navbarCollapse.classList.contains('show')) {
-        new Collapse(navbarCollapse).hide(); // Utilise l'objet Collapse pour fermer le menu
-      }
+      this.isMenuOpen = false;
     },
 
     handleOutsideClick(event) {
@@ -65,6 +73,11 @@ import { Collapse } from 'bootstrap'; // Import explicite de Collapse
 
 <style lang="scss" scoped>
 
+.dropdown{
+  display: flex;
+  align-items: center;
+}
+
 #nav {
   background-color: #fcfcf7;
     z-index: 10;
@@ -73,8 +86,19 @@ import { Collapse } from 'bootstrap'; // Import explicite de Collapse
     top: 0;
     width: 100%; 
    }
+
+   .nav-link-projects{
+    font-family: "Istok Web", sans-serif;
+  font-weight: 500;
+  font-style: normal;
+    color: black;
+    font-size: 1.5em;
+    margin-right: 40px;
+    text-decoration: none;
+   }
   
   .nav-link {
+    position: relative;
     font-family: "Istok Web", sans-serif;
   font-weight: 500;
   font-style: normal;
@@ -82,14 +106,41 @@ import { Collapse } from 'bootstrap'; // Import explicite de Collapse
     font-size: 1.5em;
     margin-right: 40px;
   }
+
+  /* Style pour la ligne sous le lien */
+.nav-link::after {
+  content: '';
+  position: absolute;
+  left: 0;
+  bottom: -1px; /* Positionnez la ligne sous le texte */
+  width: 100%;
+  height: 1px; /* Épaisseur de la ligne */
+  background-color: black; /* Couleur de la ligne */
+  transform: scaleX(0); /* Ligne invisible au départ */
+  transform-origin: right; /* Point de départ de l’animation */
+  transition: transform 0.5s ease; /* Animation d’extension de la ligne */
+}
+
+/* Style au survol */
+.nav-link:hover {
+  color: #333; /* Change la couleur du texte au survol */
+}
+
+/* Animation de la ligne au survol */
+.nav-link:hover::after {
+  transform: scaleX(1); /* Fait apparaître la ligne en l’étendant */
+  transform-origin: left; /* Étend la ligne de gauche à droite */
+}
   
-  // .nav-link:hover {
-  //   color: black;
-  // }
-  
-  // .nav-link:active {
-  //   color: black;
-  // }
+/* Applique le soulignement permanent sur le lien actif */
+.router-link-exact-active::after {
+  transform: scaleX(1);
+  transform-origin: left;
+}
+
+.no-underline::after {
+  display: none;
+}
   
   .link-cv {
     border: 2.5px solid black;
@@ -108,6 +159,18 @@ import { Collapse } from 'bootstrap'; // Import explicite de Collapse
   .coll-header{
     background-color: #fcfcf7;
     padding-bottom: 30px;
+  }
+  .navbar-toggler{
+    margin-bottom: 40px;
+  }
+
+  .nav-link:hover::after{
+    display: none;
+  }
+
+  .router-link-exact-active::after{
+    width: 18%;
+    padding: 0px;
   }
 }
   
