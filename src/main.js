1fe5e732154ec71d850aaedf2a21e45fd2e8ceec
Victor <victordevos.pro@gmail.com>
2026-08-04 16:59:03 +0200

    install gsap

--- src/main.js
+++ src/main.js
@@ -16,6 +16,21 @@ import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import '@mdi/font/css/materialdesignicons.css'
 import '@/assets/styles/main.scss';
+// GSAP
+import { gsap } from "gsap";
+
+import { CustomEase } from "gsap/CustomEase";
+// CustomWiggle requires CustomEase
+import { CustomWiggle } from "gsap/CustomWiggle";
+    
+import { Flip } from "gsap/Flip";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+// ScrollSmoother requires ScrollTrigger
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(Flip,ScrollTrigger,ScrollSmoother,ScrollToPlugin,SplitText,CustomEase,CustomWiggle);
 
 const vuetify = createVuetify({
   components,
